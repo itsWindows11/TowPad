40858ab6c46369d4839542540a9b8909e7990273
itsWindows11 <81253203+itsWindows11@users.noreply.github.com>
2021-09-13 21:03:06 +0200

    Edit assembly title/product

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rich Text Editor")]
+[assembly: AssemblyTitle("Wordpad UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rich Text Editor")]
+[assembly: AssemblyProduct("Wordpad UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
